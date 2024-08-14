e0e7943ab73ca234d50601d5153d5e0bb18a4285
ColorWizardd <colorwizardd.biz@gmail.com>
2024-08-13 21:53:02 -0500

    Created display.html  - Created basic controls to add inits

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+/*
 const { WebSocketServer } = require('ws');
 const wss = new WebSocketServer( {port: 21000} );
+
+ -- Websocket Functionality -- 
 
 wss.on('connection', function connection(ws){
     console.log(`New client connected!`);
@@ -12,3 +15,57 @@ wss.on('connection', function connection(ws){
 
     });
 });
+*/
+
+
+/* --  Innit List Functionality -- */
+
+const innitArr = [];
+
+
+async function sortedInsert(){
+    const innitList = document.getElementById("list-container");
+    let nameBox = document.getElementById("innitName");
+    let initBox = document.getElementById("innitNum");
+    let hiddenCheck = document.getElementById("innitHide");
+    let innitName = nameBox.value;
+    let innitNum = initBox.value;
+    let isHidden = hiddenCheck.checked;
+    const newItem = {
+            "name" : innitName,
+            "num" : innitNum,
+            "hidden" : isHidden
+    }
+    
+    console.log(`Adding value ${newItem.num} to array`);
+
+    /* TODO: FIGURE OUT HOW TO DO A SORTED INSERT WHILE MAINTAINING INIT ID ORDER */
+
+    
+}
+
+
+async function listSearch(newItem){
+    let start = 0;
+    let end = innitArr.length - 1;
+    let newVal = newItem.num;
+
+    if(start == end){
+        return start;
+    }
+
+    while(innitArr[start].num < innitArr[end].num){
+        let mid = Math.floor((start + end) / 2);
+        if(innitArr[mid].num < newVal){
+            start = mid + 1;
+        }
+        else{
+            high = mid;
+        }
+        return start;
+    }
+}
+
+async function addItem(){
+    await sortedInsert();
+}
